6dffc15c74a3ee8f6962873cc886e1f04b8793d7
Mbahtata <107659353+Mbahtata@users.noreply.github.com>
2025-12-11 19:56:54 -0800

    Refactor RDS Terraform configuration
    Removed sensitive variable definitions and used hardcoded values for username and password.

--- rds-terraform.tf
+++ rds-terraform.tf
@@ -3,22 +3,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "db_username" {
-  type      = string
-  sensitive = true
-  default = "maloke"
-}
-
-variable "db_password" {
-  type      = string
-  sensitive = true
-  default = "admin123"
-}
-
-db_username = "maloke"
-
-db_password = "admin123"
-
 resource "aws_db_subnet_group" "this" {
   name       = "rds-postgres-subnet-group"
   subnet_ids = ["subnet-123", "subnet-456"]  # update
@@ -34,8 +18,8 @@ resource "aws_db_instance" "postgres" {
   engine_version          = "16.4"
   instance_class          = "db.t3.micro"
   allocated_storage       = 20
-  username                = var.db_usernam
-  password                = var.db_password
+  username                = "maloke"
+  password                = "admin123"
   vpc_security_group_ids  = ["sg-123456789"]  # update
   db_subnet_group_name    = aws_db_subnet_group.this.name
   skip_final_snapshot     = true
