4c86cc28157982d4fcff388a360689f93956867a
Mbahtata <107659353+Mbahtata@users.noreply.github.com>
2025-12-11 20:09:46 -0800

    Upgrade PostgreSQL engine version to 16.6

--- rds-terraform.tf
+++ rds-terraform.tf
@@ -102,7 +102,7 @@ resource "aws_security_group" "rds" {
 resource "aws_db_instance" "postgres" {
   identifier                 = "my-postgres-db"
   engine                     = "postgres"
-  engine_version             = "16.3"
+  engine_version             = "16.6"
   instance_class             = "db.t3.micro"
   allocated_storage          = 20
   username                = "maloke"
